f3b7fe112fbcf3cec725ad1c58fe64319228494a
pandacat <pandacat@Lenovo>
2017-03-02 13:33:46 -0800

    Added vgg download script

---
+++ examples/vgg_extractor/get_vgg_16.sh
@@ -0,0 +1,2 @@
+wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_layers.caffemodel
+wget https://gist.githubusercontent.com/ksimonyan/211839e770f7b538e2d8/raw/0067c9b32f60362c74f4c445a080beed06b07eb3/VGG_ILSVRC_16_layers_deploy.prototxt
