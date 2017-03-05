8ab4ab43587f04360f02fc34f9b34f9d7b6a6aaa
priyankar10 <priyankaraviv@gmail.com>
2017-03-05 15:25:36 -0800

    Add files via upload

---
+++ examples/read_json.py
@@ -0,0 +1,38 @@
+import json
+import bz2
+import nltk.data
+import re
+import os
+import sys
+import string
+
+f=open('Reddit_data_sentences.txt','w')
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+for file in os.listdir(sys.argv[0]):
+	filepath=sys.argv[0]+file
+	contents=bz2.BZ2File(filepath, "r")
+	for sent_read in contents:
+		data = json.loads(sent_read)
+		try:
+			sent_content=str(data['body'])
+			sent_content=sent_content.strip()
+			if sent_content=='[deleted]':
+				continue
+			#print sent_content,"\n"
+			sentence_list=tokenizer.tokenize(sent_content)
+			for sentence in sentence_list:
+				if sentence.isspace():
+					continue
+				for ch in ['(',')','[',']','{','}','\'','\"','\n','\r']:
+					sentence=sentence.replace(ch,'')
+				sentence=(sentence.lower()).rstrip('?:!.,;')
+				if sentence.isspace():
+					continue
+				f.write(sentence)
+				f.write('\n')
+				#print line
+		except (UnicodeDecodeError, UnicodeEncodeError) as e:
+			continue
+f.close()
+
